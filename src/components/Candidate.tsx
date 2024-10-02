c3c77bb52f546eb436e9a3c729a5b083c1843fff
Heisenberg <esfahani.cyrus@gmail.com>
2024-10-02 16:10:26 -0700

    added api updates along with candidate updates

---
+++ src/components/Candidate.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Candidate as CandidateType } from '../interfaces/Candidate.interface';
+
+interface CandidateProps {
+  candidate: CandidateType;
+  onSave: () => void;
+  onSkip: () => void;
+}
+
+const Candidate: React.FC<CandidateProps> = ({ candidate, onSave, onSkip }) => {
+  return (
+    <div>
+      <img src={candidate.avatar_url} alt="avatar" />
+      <h2>{candidate.name}</h2>
+      <p>{candidate.username}</p>
+      <p>{candidate.location}</p>
+      <p>{candidate.email}</p>
+      <p>{candidate.company}</p>
+      <a href={candidate.html_url}>GitHub Profile</a>
+      <button onClick={onSave}>+</button>
+      <button onClick={onSkip}>-</button>
+    </div>
+  );
+};
+
+export default Candidate;
